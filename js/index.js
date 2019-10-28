8c35731b4d136b3acd294abbfe5128b196547e0c
blakelower <blakelower1999@gmail.com>
2019-10-28 21:23:32 +0700

    project, didnt know how to append

--- js/index.js
+++ js/index.js
@@ -26,6 +26,9 @@ const siteContent = {
     "vision-h4":"Vision",
     "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
+
+
+  
   "contact": {
     "contact-h4" : "Contact",
     "address" : "123 Way 456 Street Somewhere, USA",
@@ -47,3 +50,44 @@ for (let i=0; i < navLinks.length; i++){
 }
 
 
+let green= document.querySelectorAll("header nav a");
+green.forEach(aGreen => {
+  aGreen.style.color = "green";
+});
+
+const topHeader = document.querySelector('h1');
+topHeader.textContent = siteContent.cta.h1;
+
+const topButton = document.querySelector('button');
+topButton.textContent = siteContent.cta.button;
+const ctaButton = document.querySelector('.cta-text button');
+ctaButton.textContent = siteContent.cta.button;
+
+let topContentH4 = document.querySelectorAll('.main-content .top-content H4');
+topContentH4[0].textContent = siteContent['main-content']['features-h4']
+topContentH4[1].textContent = siteContent['main-content']['about-h4']
+
+let topContentP = document.querySelectorAll('.main-content .top-content p');
+topContentP[0].textContent = siteContent['main-content']['features-content']
+topContentP[1].textContent = siteContent['main-content']['about-content']
+
+let bottomContentH4 = document.querySelectorAll('.main-content .bottom-content h4');
+bottomContentH4[0].textContent = siteContent['main-content']['services-h4']
+bottomContentH4[1].textContent = siteContent['main-content']['product-h4']
+bottomContentH4[2].textContent = siteContent['main-content']['vision-h4']
+
+let bottomContentP = document.querySelectorAll('.main-content .bottom-content p');
+bottomContentP[0].textContent = siteContent['main-content']['services-content']
+bottomContentP[1].textContent = siteContent['main-content']['product-content']
+bottomContentP[2].textContent = siteContent['main-content']['vision-content']
+
+const contactHeader = document.querySelectorAll('.contact h4');
+contactHeader[0].innerText = siteContent.contact['contact-h4'];
+
+const contactContent = document.querySelectorAll('.contact p');
+contactContent[0].textContent = siteContent.contact.address
+contactContent[1].textContent = siteContent.contact.phone
+contactContent[2].textContent = siteContent.contact.email
+
+const footer = document.querySelector('footer');
+footer.textContent = siteContent.footer.copyright;
